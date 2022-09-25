aa1140b4d3ebffcce8a41a8d5badcad708aa9047
Donice Ubaru <doniceubaru1@gmail.com>
2022-09-25 13:11:46 +0100

    Styled the Service component

--- src/components/pages/home/Services.tsx
+++ src/components/pages/home/Services.tsx
@@ -23,22 +23,22 @@ const Services = () => {
   return (
     <div className="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
       <h2 className="text-2xl font-bold tracking-tight text-white sm:text-3xl md:text-3xl">Breef for Talents & Creatives</h2>
-      <p className="mt-3 text-base text-gray-200 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">
+      <p className=" mt-3 text-base text-gray-200 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">
         Get Detailed Briefings and Seamless payments for the work you love
       </p>
 
-      <div className="mx-auto mt-10 max-w-7xl px-0 sm:flex sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-        <div className='glassmorphic-div w-full mx-auto mt-4 max-w-7xl px-4 py-10 sm:w-[30%] sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28'>
+      <div className="my-auto mt-5 max-w-7xl sm:flex sm:mt-12 md:mt-16 lg:mt-20 xl:mt-5">
+        <div className='glassmorphic-div w-full mx-auto max-w-7xl px-4 py-10 sm:w-[30%] sm:px-6'>
           <h1 className='text-xl mx-2font-light text-teal-400'>Get Paid Globally</h1>
           <p className='text-gray-50 font-light'>With Breef, you would be able to take on jobs from foreigners without the payment barriers</p>
         </div>
 
-        <div className='glassmorphic-div w-full mx-auto mt-4 max-w-7xl px-4 py-10 sm:w-[30%] sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28'>
+        <div className='glassmorphic-div w-full mt-4 mx-auto max-w-7xl px-4 py-10 sm:w-[30%] sm:px-6'>
           <h1 className='text-xl mx-2font-light text-teal-400'>Get Paid Globally</h1>
           <p className='text-gray-50 font-light'>With Breef, you would be able to take on jobs from foreigners without the payment barriers</p>
         </div>
 
-        <div className='glassmorphic-div w-full mx-auto mt-4 max-w-7xl px-4 py-10 sm:w-[30%] sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28'>
+        <div className='glassmorphic-div w-full mt-4 mx-auto max-w-7xl px-4 py-10 sm:w-[30%] sm:px-6'>
           <h1 className='text-xl mx-2font-light text-teal-400'>Get Paid Globally</h1>
           <p className='text-gray-50 font-light'>With Breef, you would be able to take on jobs from foreigners without the payment barriers</p>
         </div>
--- src/components/static/Footer.tsx
+++ src/components/static/Footer.tsx
@@ -3,8 +3,33 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-      Footer
+    <div className=" mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+      <hr />
+      <div className='flex text-teal-500 mt-10'>
+        <div className='w-full'>
+          <a href="#">
+            <span className="sr-only">Your Company</span>
+            <h1
+              className="logo h-8 w-auto sm:h-10 text-yellow-400"
+            > Breef. </h1>
+          </a>
+        </div>
+
+        <div>
+          <p>Building this from the Trenches of Ekosodin, Benin City, Ngeria</p>
+        </div>
+
+        <div>
+          <p>doniceubaru@Breef.com</p>
+        </div>
+
+        <div>
+          <a href="https://www.instagram.com/drdonice">Instagram</a>
+          <a href="https://www.twitter.com/drdonice">Twitter</a>
+          <a href="https://www.linkedin.com/in/doniceubaru">LinkedIn</a>
+        </div>
+
+      </div>
     </div>
   )
 }
