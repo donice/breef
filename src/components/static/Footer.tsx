766bea53836794fa398c7882534c6dee050205ad
Donice Ubaru <doniceubaru1@gmail.com>
2022-09-25 13:21:25 +0100

    Edited the footer

--- src/components/static/Footer.tsx
+++ src/components/static/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className=" mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
       <hr />
-      <div className='flex text-teal-500 mt-10'>
+      <div className='text-teal-50 mt-10 text-xs'>
         <div className='w-full'>
           <a href="#">
             <span className="sr-only">Your Company</span>
@@ -24,8 +24,8 @@ const Footer = () => {
         </div>
 
         <div>
-          <a href="https://www.instagram.com/drdonice">Instagram</a>
-          <a href="https://www.twitter.com/drdonice">Twitter</a>
+          <a href="https://www.instagram.com/drdonice">Instagram  </a>
+          <a href="https://www.twitter.com/drdonice">Twitter  </a>
           <a href="https://www.linkedin.com/in/doniceubaru">LinkedIn</a>
         </div>
 
