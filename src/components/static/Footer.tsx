8604b083742276fb2cd70dfedef42aa2546bdd2f
Donice Ubaru <doniceubaru1@gmail.com>
2022-09-25 13:25:05 +0100

    Edited Footer

--- src/components/static/Footer.tsx
+++ src/components/static/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className=" mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
       <hr />
-      <div className='text-teal-50 mt-10 text-xs'>
+      <div className='text-teal-50 mt-10 pb-10 text-xs'>
         <div className='w-full'>
           <a href="#">
             <span className="sr-only">Your Company</span>
@@ -15,15 +15,15 @@ const Footer = () => {
           </a>
         </div>
 
-        <div>
+        <div className='mb-2'>
           <p>Building this from the Trenches of Ekosodin, Benin City, Ngeria</p>
         </div>
 
-        <div>
+        <div className='mb-2'>
           <p>doniceubaru@Breef.com</p>
         </div>
 
-        <div>
+        <div className=''>
           <a href="https://www.instagram.com/drdonice">Instagram  </a>
           <a href="https://www.twitter.com/drdonice">Twitter  </a>
           <a href="https://www.linkedin.com/in/doniceubaru">LinkedIn</a>
