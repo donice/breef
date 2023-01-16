aaa8dad31ae9d0f2e21b7e0059dd397028695b9c
drdonice <87942465+donice@users.noreply.github.com>
2023-01-16 23:58:34 +0100

    Update Account.tsx

--- src/components/pages/Account.tsx
+++ src/components/pages/Account.tsx
@@ -4,7 +4,7 @@ const Account = () => {
   return (
     <div className='max-w-[600px] mx-auto my-16 p-4'>
       <h1 className='text-2xl font-bold py-4'>My Account</h1>
-      <p>User Email:</p>
+      <p>User email:</p>
 
       <button className='border px-6 py-2 my-4'>Log Out</button>
     </div>
